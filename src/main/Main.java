c442e8ffc883d244c3e5ea0cc894e13a5c14b045
haru-ish <haru-ish@j0d.de>
2023-03-28 19:40:55 +0200

    Change codes having row and column to position

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,24 +7,24 @@ import main.FlexibleTicTacToe.WinStatus;
 public class Main {
 
 	public static void main(String[] args) throws TilesNumberException, PositionNumberException {
-		// players decide the number of tiles
-		FlexibleTicTacToe tictactoe = new FlexibleTicTacToe(3);
+		// decide the number of tiles(row, column) and how many in a row are required to win
+		FlexibleTicTacToe tictactoe = new FlexibleTicTacToe(3,4,3);
+		// set up the player
+		Player player = Player.PLAYER1;
 		// receive the game's result (who is winner)
 		WinStatus result = WinStatus.INPROGRESS;
-		Player player = Player.PLAYER1;
 		// loop until one of them wins
 		while (result == WinStatus.INPROGRESS) {
 			Scanner scan = new Scanner(System.in);
-			System.out.println(player.toString() + "'s turn: Enter row and column");
-			// a tile has row and column
-			int row = scan.nextInt();
-			int column = scan.nextInt();
+			System.out.println(player.toString() + "'s turn: Enter your placement (1-" + 3*4 + ")");
+			int position = scan.nextInt();
 			// the player select a position (row and column) to put the symbol
 			// check if this turn the player wins or not
-			result = tictactoe.playerSelectPosition(row, column, player);
+			result = tictactoe.playerSelectPosition(position, player);
 			// change player
 			player = player.next();
 		}
+		// for print out
 		if(result == WinStatus.DRAW) {
 			System.out.println("Outcome: this game is a " + result.toString());
 		} else {
