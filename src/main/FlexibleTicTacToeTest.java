9e8ba932abfedda4ef9fe13f7e08d2b56fc320f0
haru-ish <haru-ish@j0d.de>
2023-04-23 18:28:46 +0200

    Fix some codes that reflect code review

--- src/main/FlexibleTicTacToeTest.java
+++ src/main/FlexibleTicTacToeTest.java
@@ -1,6 +1,5 @@
 package main;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,92 +14,93 @@ import org.junit.jupiter.api.TestMethodOrder;
 import java.util.Arrays;
 import java.util.List;
 
-
 @TestMethodOrder(OrderAnnotation.class)
 class FlexibleTicTacToeTest {
+	@Test
+	@DisplayName("check symbol of enum Player")
+	public void checkPlayerSymbol() {
+		assertEquals(Player.PLAYER1.getSymbol(), 'o');
+		assertEquals(Player.PLAYER2.getSymbol(), 'x');
+	}
+
 	@Test
 	@DisplayName("check next method of enum Player")
 	public void checkPlayerNext() {
 		assertEquals(Player.PLAYER1.next(), Player.PLAYER2);
 		assertEquals(Player.PLAYER2.next(), Player.PLAYER1);
 	}
+
+	@Test
+	@DisplayName("check winStatus method of enum Player")
+	public void checkPlayerToWinStatus() {
+		assertEquals(Player.PLAYER1.toWinStatus(), WinStatus.PLAYER1);
+		assertEquals(Player.PLAYER2.toWinStatus(), WinStatus.PLAYER2);
+	}
+
 	@Test
 	@DisplayName("check to throw TilesNumberException If any of the row, column, or the winnnig row is 0")
 	void checkThrowTilesNumberException1() {
-		Throwable exception = assertThrows(TilesNumberException.class, () -> {
-			new FlexibleTicTacToe(0,0,0);
+		assertThrows(TilesNumberException.class, () -> {
+			new FlexibleTicTacToe(0, 0, 0);
 		});
-		assertEquals(exception.getMessage(), "Enter a number more than 0");
 	}
-	
+
 	@Test
 	@DisplayName("check to throw TilesNumberException if the winning row is larger than the row and column")
 	void checkThrowTilesNumberException2() {
-		Throwable exception = assertThrows(TilesNumberException.class, () -> {
-			new FlexibleTicTacToe(1,1,3);
+		assertThrows(TilesNumberException.class, () -> {
+			new FlexibleTicTacToe(1, 1, 3);
 		});
-		assertEquals(exception.getMessage(), "Set the number of a row to win less than " + 3);
 	}
-	
+
 	@Test
 	@DisplayName("check not to throw TilesNumberException")
 	void checkDoesNotThrowTilesNumberException() throws TilesNumberException {
-		assertDoesNotThrow(() -> new FlexibleTicTacToe(1,1,1));
+		assertDoesNotThrow(() -> new FlexibleTicTacToe(1, 1, 1));
 	}
 
 	@Test
 	@DisplayName("check to make game board properly")
-	void testMakeGameBoard() throws TilesNumberException {
+	void testMakeGameBoard() throws TilesNumberException, PositionNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		char[][] tiles = flex.makeGameBoard(3, 4, 2);
-		assertEquals(tiles[3-1][4-1], ' ');
-		
+		flex.playerSelectPosition(10, Player.PLAYER1);
+		char[][] tiles = flex.makeGameBoard();
+		assertEquals(tiles[(10-1) / 4][(10-1) % 4], Player.PLAYER1.getSymbol());
 	}
 
 	@Test
 	@DisplayName("check to throw PositionNumberException if the position does not exist")
 	void checkThrowPositionNumberException1() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		Throwable exception = assertThrows(PositionNumberException.class, () -> {
-			flex.checkThePosition(13);
+		assertThrows(PositionNumberException.class, () -> {
+			flex.playerSelectPosition(13, Player.PLAYER1);
 		});
-		assertEquals(exception.getMessage(), "Enter a number less than " + 13);
 	}
-	
+
 	@Test
 	@DisplayName("check to throw PositionNumberException if the number of position is 0")
 	void checkThrowPositionNumberException2() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		Throwable exception = assertThrows(PositionNumberException.class, () -> {
-			flex.checkThePosition(0);
+		assertThrows(PositionNumberException.class, () -> {
+			flex.playerSelectPosition(0, Player.PLAYER1);
 		});
-		assertEquals(exception.getMessage(), "Enter a number more than 0");
 	}
-	
+
 	@Test
 	@DisplayName("check to throw PositionNumberException when the position is already selected")
 	void checkThrowPositionNumberException3() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		flex.checkThePosition(3);
-		Throwable exception = assertThrows(PositionNumberException.class, () -> {
-			flex.checkThePosition(3);
+		flex.playerSelectPosition(3, Player.PLAYER1);
+		assertThrows(PositionNumberException.class, () -> {
+			flex.playerSelectPosition(3, Player.PLAYER2);
 		});
-		assertEquals(exception.getMessage(), "You cannot select the position because it is already selected");
 	}
 	
 	@Test
 	@DisplayName("check not to throw PositionNumberException")
 	void checkDoesNotThrowPositionNumberException() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		assertDoesNotThrow(() -> flex.checkThePosition(12));
-	}
-	
-	@Test
-	@DisplayName("check selected position is add to the list")
-	void testCheckThePosition() throws PositionNumberException, TilesNumberException {
-		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		List<Integer> list= flex.checkThePosition(12);
-		assertTrue(list.contains(12));
+		assertDoesNotThrow(() -> flex.playerSelectPosition(3, Player.PLAYER1));
 	}
 
 	@Test
@@ -115,11 +115,11 @@ class FlexibleTicTacToeTest {
 	@DisplayName("check to return INPROGRESS for player2")
 	void checkReturnInprogress2() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(3, 4, 2);
-		flex.playerSelectPosition(1, Player.PLAYER1);
+		flex.playerSelectPosition(9, Player.PLAYER1);
 		WinStatus result = flex.playerSelectPosition(2, Player.PLAYER2);
 		assertEquals(result, WinStatus.INPROGRESS);
 	}
-	
+
 	@Test
 	@DisplayName("check to return Player1 if the winning row is 1")
 	void checkReturnPlay1WithTheWinningRow1() throws PositionNumberException, TilesNumberException {
@@ -127,48 +127,47 @@ class FlexibleTicTacToeTest {
 		WinStatus result = flex.playerSelectPosition(3, Player.PLAYER1);
 		assertEquals(result, WinStatus.PLAYER1);
 	}
-	
+
 	@Test
 	@DisplayName("check to return Player1 with vertical winning condition")
 	void checkReturnPlayer1() throws PositionNumberException, TilesNumberException {
-		FlexibleTicTacToe flex = new FlexibleTicTacToe(2, 6, 3);
+		FlexibleTicTacToe flex = new FlexibleTicTacToe(6, 2, 3);
 		flex.playerSelectPosition(1, Player.PLAYER1);
 		flex.playerSelectPosition(9, Player.PLAYER2);
 		flex.playerSelectPosition(3, Player.PLAYER1);
 		flex.playerSelectPosition(4, Player.PLAYER2);
 		WinStatus result = flex.playerSelectPosition(5, Player.PLAYER1);
-		
 		assertEquals(result, WinStatus.PLAYER1);
 	}
-	
+
 	@Test
 	@DisplayName("check to return Player2 with diagonal winning condition")
 	void checkReturnPlayer2() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(5, 3, 2);
-		flex.playerSelectPosition(3, Player.PLAYER1);
-		flex.playerSelectPosition(1, Player.PLAYER2);
+		flex.playerSelectPosition(6, Player.PLAYER1);
+		flex.playerSelectPosition(15, Player.PLAYER2);
 		flex.playerSelectPosition(10, Player.PLAYER1);
-		WinStatus result = flex.playerSelectPosition(7, Player.PLAYER2);
-		
+		WinStatus result = flex.playerSelectPosition(11, Player.PLAYER2);
+
 		assertEquals(result, WinStatus.PLAYER2);
 	}
-	
+
 	@Test
 	@DisplayName("check to return Player1 with horizontal winning condition")
 	void checkReturnPlayer1WithHorizontal() throws PositionNumberException, TilesNumberException {
 		FlexibleTicTacToe flex = new FlexibleTicTacToe(4, 4, 2);
-		flex.playerSelectPosition(1, Player.PLAYER1);
+		flex.playerSelectPosition(11, Player.PLAYER1);
 		flex.playerSelectPosition(16, Player.PLAYER2);
-		WinStatus result = flex.playerSelectPosition(2, Player.PLAYER1);
-		
+		WinStatus result = flex.playerSelectPosition(10, Player.PLAYER1);
+
 		assertEquals(result, WinStatus.PLAYER1);
 	}
-	
+
 	@Test
 	@DisplayName("check to return Draw")
 	void checkReturnDraw() throws PositionNumberException, TilesNumberException {
-		FlexibleTicTacToe flex = new FlexibleTicTacToe(5,4,5);
-		List<Integer> positions = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+		FlexibleTicTacToe flex = new FlexibleTicTacToe(2, 3, 3);
+		List<Integer> positions = Arrays.asList(1, 2, 3, 4, 5, 6);
 		Player player = Player.PLAYER1;
 		WinStatus result = WinStatus.INPROGRESS;
 		for (Integer position : positions) {
